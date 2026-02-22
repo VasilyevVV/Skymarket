5b2493e7b92c980712ff03a3c65eff1d3250c4b4
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2026-02-22 22:54:12 +0700

    Added creation of objects from a JSON-file

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+
+from src.Product import Product
+from src.Category import Category
+
+
+def read_json(path: str) -> dict:
+    """Функция загружает данные из файла в формате JSON и преобразует их в словарь (dict)"""
+    full_path = os.path.abspath(path)
+    try:
+        with open(full_path, "r", encoding="UTF-8") as file:
+            data_dict = json.load(file)
+            return data_dict
+    except FileNotFoundError:
+        return {}
+
+
+def create_objects_from_json(dict_data):
+    """
+    Функция принимает данные в виде словаря и создаёт объекты - экземпляры классов Category и Product
+    на основе чтения данных из словаря.
+    """
+    if dict_data != {}:
+        # пустой список для хранения объектов Category
+        categories = []
+        # запускаем цикл по категориям
+        for category in dict_data:
+            # пустой список для хранения объектов Product
+            products = []
+            # цикл по продуктам в категории
+            for product in category["products"]:
+                # Распаковка значений словаря - создание объекта Product и добавление его в список.
+                products.append(Product(**product))
+            # обновление списка продуктов для категории
+            category["products"] = products
+            # создание объекта Category и добавление его в список
+            categories.append(Category(**category))
+        return categories
